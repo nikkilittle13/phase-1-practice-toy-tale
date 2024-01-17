d93c4dcb8113160913024c8f9b9ffc5375853f29
Nikki Little <nikkilittle13@gmail.com>
2024-01-17 15:42:21 -0500

    finished lab

--- src/index.js
+++ src/index.js
@@ -12,4 +12,83 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  document.querySelector(".add-toy-form").addEventListener("submit", handleSubmit);
 });
+
+function addToyToCard(toyObj) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(toyObj),
+  })
+    .then((resp) => resp.json())
+    .then(data => console.log(data))
+};
+
+function handleSubmit(e){
+  e.preventDefault();
+  let toyObj = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+  renderOneToy(toyObj);
+  addToyToCard(toyObj);
+}
+
+function renderOneToy(toy) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img class="toy-avatar" src="${toy.image}">
+    <p>${toy.likes} Likes</p>
+    <div class="like-btn" id="${toy.id}">
+      <button>Like ❤️</button>
+    </div>
+  `;
+
+  const likeBtns = card.querySelectorAll(".like-btn");
+  likeBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      toy.likes++;
+      card.querySelector('p').textContent = `${toy.likes} Likes`;
+      updateToy(toy);
+    });
+  });
+
+  document.querySelector('#toy-collection').appendChild(card);
+};
+
+function updateToy(toyObj) {
+  fetch(`http://localhost:3000/toys/${toyObj.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": toyObj.likes
+    })
+  })  
+  .then(resp => resp.json())
+  .then(toy => {
+    document.querySelector('p').textContent = `${toy.likes} Likes`;
+  });
+};
+
+
+function getToys() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(data => data.forEach(toy => renderOneToy(toy)))
+};
+
+function initialize(){
+  getToys();
+}
+initialize();
